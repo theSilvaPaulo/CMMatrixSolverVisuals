c47a489652ac172e00c6aec3f6775023986b2d23
Paulo <paulo99.h.silva@gmail.com>
2023-07-09 09:24:27 -0300

    working main

--- cm-matrix-solver-visuals/src/main.js
+++ cm-matrix-solver-visuals/src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+
+import { createRouter, createWebHistory } from 'vue-router';
+import routes from './router';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
